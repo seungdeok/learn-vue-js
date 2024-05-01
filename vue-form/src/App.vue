9a700e6f2ef7242382dcc9c671e35c75fb7cf0e4
seungdeok <akk9132@naver.com>
2024-05-01 11:07:41 +0900

    feat: Add section12

---
+++ vue-form/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <form v-on:submit.prevent="submitForm">
+    <div>
+      <label for="username">id:</label>
+      <input id="username" type="text" v-model="username" />
+    </div>
+    <div>
+      <label for="password">pw:</label>
+      <input id="password" type="password" v-model="password" />
+    </div>
+    <button type="submit">Login</button>
+  </form>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    submitForm: function () {
+      // event.preventDefault(); // 새로고침을 막는다
+      console.log({
+        username: this.username,
+        password: this.password,
+      });
+
+      const url = "https://jsonplaceholder.typicode.com/users";
+      const data = {
+        username: this.username,
+        password: this.password,
+      };
+      axios
+        .post(url, data)
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
+
+<style></style>
